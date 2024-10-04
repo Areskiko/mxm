bd7416823f46bb9c948173223dfca770ac86d04c
Stefan Mack <developer@mack.aleeas.com>
2024-10-04 13:52:00 +0200

    Functioning tiling implementation

--- mxm.c
+++ mxm.c
@@ -14,8 +14,8 @@ DATA_TYPE *mxm(DATA_TYPE *A, DATA_TYPE *B, HEADER_TYPE N) {
   size_t blockNum = N / BLOCK_SIZE;
   DATA_TYPE *C = calloc(N * N, sizeof(DATA_TYPE));
 
-// Traverse blocks.
-#pragma omp parallel for
+  // Traverse blocks.
+  // #pragma omp parallel for
   for (size_t bi = 0; bi < blockNum; bi++) {
     for (size_t bj = 0; bj < blockNum; bj++) {
 
@@ -42,7 +42,7 @@ DATA_TYPE *mxm(DATA_TYPE *A, DATA_TYPE *B, HEADER_TYPE N) {
         // Block GEMM.
         for (size_t i = 0; i < BLOCK_SIZE; i++) {
           for (size_t k = 0; k < BLOCK_SIZE; k++) {
-#pragma omp simd
+            // #pragma omp simd
             for (size_t j = 0; j < BLOCK_SIZE; j++) {
               localC[i][j] += localA[i][k] * localB[k][j];
             }
